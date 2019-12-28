40d5aec5b0830b8c092068ed18eb8e099259bf35
kimeunhye224 <kimeunhye@gmail.com>
2019-12-28 12:50:46 +0400

    add maxMultiple

---
+++ Katas/7kyu/maxMultiple.js
@@ -0,0 +1,39 @@
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+// Conditions :
+// N is divisible by divisor
+
+// N is less than or equal to bound
+
+// N is greater than 0.
+
+// Notes
+// The parameters (divisor, bound) passed to the function are only positve values .
+// It's guaranteed that a divisor is Found .
+
+function maxMultiple(divisor, bound) {
+    var arrayOfMultiples = [];
+        for (var N = 0; N <= bound; N++) {
+            if (isDivisibleByDivisor(N, divisor)) {
+                arrayOfMultiples.push(N);
+            }
+        }
+    return largestIntegerInAnArray(arrayOfMultiples);
+}
+
+isDivisibleByDivisor = (N, divisor) => N % divisor === 0; 
+
+largestIntegerInAnArray = arr => Math.max(...arr);
+
+
+//test
+
+console.log(maxMultiple(2,7)) //6
+console.log(maxMultiple(10, 50)) //50
+console.log(maxMultiple(37, 200)) //185
+
+//other solutions
+
+function maxMultiple(divisor, bound){
+    return bound-bound%divisor
+  }
