5e946a455cff5d6ace4652fa414dd1981312eead
kimeunhye224 <kimeunhye224@gmail.com>
2020-10-27 21:45:02 +0900

    add solution by mentor and rename some variables

--- Katas/6kyu/oddInteger.js
+++ Katas/6kyu/oddInteger.js
@@ -3,25 +3,48 @@
  * There will always be only one integer that appears an odd number of times.
  */
 
-function findOdd(A) {
-  const newA = A.map((int) => {
-    const intAppearances = A.filter((x) => {
+//My solution (renamed variables after review)
+function findOdd1(A) {
+  const arrWithNumberAppearingOddNumberOfTimes = A.map((int) => {
+    const appearances = A.filter((x) => {
       if (x === int) return true;
     });
-    const oddNumberOfAppearances = intAppearances.length % 2 !== 0;
-    if (oddNumberOfAppearances) return int;
-  });
-
-  const undefinedFilteredA = newA.filter((int) => {
+    const appearsOddNumberOfTimes = appearances.length % 2 !== 0;
+    if (appearsOddNumberOfTimes) return int;
+  }); 
+  const numberAppearingOddNumberOfTimes = arrWithNumberAppearingOddNumberOfTimes.filter((int) => {
     if (int) return int;
   });
-  return undefinedFilteredA[0];
+  return numberAppearingOddNumberOfTimes[0];
 }
 
-console.log(findOdd([1, 1, 2, 2, 3, 3, 3]));
+console.log(findOdd1([1, 1, 2, 2, 3, 3, 3]));
 
 
-//Better way
-function findOdd(A) {
+//Too terse and cryptic for prod code 
+function findOdd2(A) {
   return A.find((item) => A.filter(el => el == item).length % 2)
 }
+
+//Solution with a 'voice'
+function findOdd3(arr) {
+  return arr.find(occursOddNumberOfTimes);
+}
+
+function occursOddNumberOfTimes(item) {
+  return isOdd(noOfOccurences(item));
+}
+
+function noOfOccurences(arr, item) {
+  return arr.filter(el => el == item) {
+  }
+}
+
+function isOdd(number) {
+  number % 2 > 0
+}
+
+
+//Tips: 
+//Solve using the problem domain -> how can I describe this problem using code?
+//Don't think about solving the whole thing at once. Get from point A to point B
