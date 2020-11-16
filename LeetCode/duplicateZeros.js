eed83b92d518428cc8c4261d0e1ce7ca9ef84981
kimeunhye224 <kimeunhye224@gmail.com>
2020-11-16 22:29:05 +0900

    WIP solve duplicateZeros

---
+++ LeetCode/duplicateZeros.js
@@ -0,0 +1,44 @@
+/**
+ * Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written.
+
+Do the above modifications to the input array in place, do not return anything from your function.
+ */
+
+ /**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    let arrLength = arr.length;
+    for (let i = 0; i < arrLength; i++) {
+      if (arr[i] === 0) {
+        arr = shiftItemsToRight(arr, i + 1);
+        console.log(arr);
+        arr[i + 1] = 0;
+      }
+    }
+};
+
+function shiftItemsToRight(arr, index) {
+  for (let i = arr.length; i  >= index; i--) {
+    arr[i + 1] = arr[i];
+  }
+  return arr; 
+}
+
+/**
+ * For each element in the array
+ * Check if the value is 0
+ * If the value = 0 
+ * Shift the next values one to the right 
+ *  For each element of the array until the index for the duplicate zero (i + 1)
+ *  array[i + 1] = array[i]
+ *  i-- 
+ * Insert extra 0 to the index + 1 
+ * 
+ */
+
+const example = [1,0,2,3,0,4,5,0];
+console.log(duplicateZeros(example))
