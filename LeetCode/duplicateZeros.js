a2cc59750589e41b92c8256f2cce61bbae9e9a0f
kimeunhye224 <kimeunhye224@gmail.com>
2020-11-17 10:24:30 +0900

    add first solution

--- LeetCode/duplicateZeros.js
+++ LeetCode/duplicateZeros.js
@@ -6,39 +6,44 @@ Note that elements beyond the length of the original array are not written.
 Do the above modifications to the input array in place, do not return anything from your function.
  */
 
- /**
+/**
  * @param {number[]} arr
  * @return {void} Do not return anything, modify arr in-place instead.
  */
-var duplicateZeros = function(arr) {
-    let arrLength = arr.length;
-    for (let i = 0; i < arrLength; i++) {
-      if (arr[i] === 0) {
-        arr = shiftItemsToRight(arr, i + 1);
-        console.log(arr);
-        arr[i + 1] = 0;
-      }
+var duplicateZeros = function (arr) {
+  let originalLength = arr.length;
+  for (let i = 0; i < originalLength; i++) {
+    if (arr[i] === 0) {
+      arr = shiftItemsToRight(arr, i + 1, originalLength);
+      console.log('arr1', arr)
+      arr[i + 1] = 0;
+      i++
     }
+  }
+
+  if (arr[arr.length - 1] === 0) {
+   arr[arr.length] = 0;
+  }
 };
 
-function shiftItemsToRight(arr, index) {
-  for (let i = arr.length; i  >= index; i--) {
-    arr[i + 1] = arr[i];
+function shiftItemsToRight(arr, index, originalLength) {
+  for (let i = originalLength; i >= index; i--) {
+      arr[i + 1] = arr[i];
   }
-  return arr; 
+  return arr;
 }
 
 /**
  * For each element in the array
  * Check if the value is 0
- * If the value = 0 
- * Shift the next values one to the right 
+ * If the value = 0
+ * Shift the next values one to the right
  *  For each element of the array until the index for the duplicate zero (i + 1)
  *  array[i + 1] = array[i]
- *  i-- 
- * Insert extra 0 to the index + 1 
- * 
+ *  i--
+ * Insert extra 0 to the index + 1
+ * Skip next index (i++)
  */
 
-const example = [1,0,2,3,0,4,5,0];
-console.log(duplicateZeros(example))
+const example = [1, 0, 2, 3, 0, 4, 5, 0];
+console.log(duplicateZeros(example));
