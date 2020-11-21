b3ec4ce7c4970d78b46c0030b6704fa8e64738d9
kimeunhye224 <kimeunhye224@gmail.com>
2020-11-21 17:48:29 +0900

    solution using two pointers

--- LeetCode/removeElement.js
+++ LeetCode/removeElement.js
@@ -6,41 +6,23 @@ Do not allocate extra space for another array, you must do this by modifying the
 
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
  */
-
 /**
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
  */
 var removeElement = function (nums, val) {
-  let length = nums.length;
-  for (let i = 0; i < length; i++) {
-    if (numberEqualsVal(nums[i], val)) {
-      nums = shiftElementsToLeft(i, nums, length)
-      length--;
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val)  {
+      nums[i] = nums[j];
+      i++;
     }
   }
 
-  return length;
+  return i;
 };
 
-function numberEqualsVal(num, val) {
-  if (num === val) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function shiftElementsToLeft(i, nums, length) {
-  for (let j = i + 1; j < length; j++) {
-    nums[j - 1] = nums[j];
-  }
-  console.log("nums", nums);
-  return nums;
-}
-
-console.log(removeElement([3, 2, 2, 3], 3));
 
 /**
  * Go through each element in nums
