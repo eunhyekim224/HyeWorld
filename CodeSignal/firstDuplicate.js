b0dd9b865f4c695f5699ecdd3d0b9d96ba2ab155
Eunhye Kim <kimeunhye224@gmail.com>
2021-05-08 18:29:50 +0100

    re-do firstduplicate

--- CodeSignal/firstDuplicate.js
+++ CodeSignal/firstDuplicate.js
@@ -2,28 +2,29 @@
   Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
  */
 
-function firstDuplicate(a) {
-  let onlyFirstDuplicateA = [];
-  let itemAndFirstDuplicateIndexA = [];
-  for (let i=0; i<a.length; i++) {
-      const duplicateIndex = a.findIndex((e, index) => e === a[i] && index !== i);
-      if (!onlyFirstDuplicateA.includes(a[i])) {
-          onlyFirstDuplicateA.push(a[i])
-          itemAndFirstDuplicateIndexA.push({item: a[i], duplicateIndex})
-      }
-  }
-  const removedNonDuplicatesA = itemAndFirstDuplicateIndexA.filter(e => e.duplicateIndex !== -1)
-  let smallestFirstDuplicate;
-  if (removedNonDuplicatesA.length > 0) {
-      let smallestIndex = a.length;
-      for (let j=0; j<removedNonDuplicatesA.length; j++) {
-          if (removedNonDuplicatesA[j].duplicateIndex < smallestIndex) {
-              smallestIndex = removedNonDuplicatesA[j].duplicateIndex;
-              smallestFirstDuplicate = removedNonDuplicatesA[j].item;
-          }
-      }
-  } else {
-      return -1
-  }
-  return smallestFirstDuplicate;
+function firstDuplicate(arr) {
+    const firstDuplicates = numberAndFirstDuplicateIndexes(arr);
+    return smallestFirstDuplicate(firstDuplicates);
 }
+
+function numberAndFirstDuplicateIndexes(arr) {
+    let onlyFirstDuplicates = [];
+    let numberAndFirstDuplicateIndexes = [];
+    for (let i = 0; i < arr.length; i++) {
+        const duplicateIndex = arr.findIndex((element, index) => element === arr[i] && index !== i);
+        if (!onlyFirstDuplicates.includes(arr[i]) && duplicateIndex !== -1) {
+            onlyFirstDuplicates.push(arr[i])
+            numberAndFirstDuplicateIndexes.push({ number: arr[i], duplicateIndex })
+        }
+    }
+    return numberAndFirstDuplicateIndexes;
+}
+
+function smallestFirstDuplicate(duplicates) {
+    const indexes = duplicates.map(duplicate => duplicate.duplicateIndex)
+    const smallestIndex = Math.min(...indexes);
+    const smallestDuplicate = duplicates.find(duplicate => duplicate.duplicateIndex === smallestIndex)
+    return smallestDuplicate.number;
+}
+
+console.log(firstDuplicate([1, 2, 5, 3, 3, 4, 4, 4, 5, 3]))
